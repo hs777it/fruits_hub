73ee8a5af9f5304da2669d11c1fe6360490adabf
Hussein Saad <hs777it@gmail.com>
2024-09-06 13:48:37 +0300

    create splash

---
+++ lib/features/splash/presentation/views/widgets/splash_view_body.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:fruits_hub/core/utils/app_images.dart';
+import 'package:svg_flutter/svg.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.end,
+          children: [
+            SvgPicture.asset(Assets.imagesPlant),
+          ],
+        ),
+        SvgPicture.asset(Assets.imagesLogo),
+        SvgPicture.asset(
+          Assets.imagesSplashBottom,
+          fit: BoxFit.fill,
+        ),
+      ],
+    );
+  }
+}
