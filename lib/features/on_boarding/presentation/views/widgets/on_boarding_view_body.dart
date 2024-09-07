7208c2cba574827ed3ec09b07c1822d71f11f403
Hussein Saad <hs777it@gmail.com>
2024-09-07 11:55:55 +0300

    add OnBoarding view

---
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_view_body.dart
@@ -0,0 +1,79 @@
+import 'package:dots_indicator/dots_indicator.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:fruits_hub/constants.dart';
+import 'package:fruits_hub/core/utils/app_colors.dart';
+import 'package:fruits_hub/core/widgets/custom_button.dart';
+import 'package:fruits_hub/features/on_boarding/presentation/views/widgets/on_boarding_page_view.dart';
+
+class OnBoardingViewBody extends StatefulWidget {
+  const OnBoardingViewBody({super.key});
+
+  @override
+  State<OnBoardingViewBody> createState() => _OnBoardingViewBodyState();
+}
+
+class _OnBoardingViewBodyState extends State<OnBoardingViewBody> {
+  late PageController pageController;
+
+  var currentPage = 0;
+  @override
+  void initState() {
+    pageController = PageController();
+
+    pageController.addListener(() {
+      currentPage = pageController.page!.round();
+      setState(() {});
+    });
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    pageController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        Expanded(
+          child: OnBoardingPageview(
+            pageController: pageController,
+          ),
+        ),
+        DotsIndicator(
+          dotsCount: 2,
+          decorator: DotsDecorator(
+            activeColor: AppColors.primaryColor,
+            color:
+                currentPage == 1 ? AppColors.primaryColor : AppColors.primaryColor.withOpacity(.5),
+          ),
+        ),
+        const SizedBox(
+          height: 29,
+        ),
+        Visibility(
+          visible: currentPage == 1 ? true : false,
+          maintainSize: true,
+          maintainAnimation: true,
+          maintainState: true,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(
+              horizontal: kHorizintalPadding,
+            ),
+            child: CustomButton(
+              onPressed: () {},
+              text: 'ابدأ الان',
+            ),
+          ),
+        ),
+        const SizedBox(
+          height: 43,
+        ),
+      ],
+    );
+  }
+}
